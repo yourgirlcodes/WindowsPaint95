4f26b6cb59ad70cae2d1d0f3b3437c36af5e8610
Zoe Cohen <zoecohen01@gmail.com>
2019-05-31 00:08:08 +0200

    updates on brush sizes and brush shapes

--- js/assignment4.js
+++ js/assignment4.js
@@ -1,6 +1,10 @@
 
-
 var colorArray = ["yellow", "red", "blue", "black", "green", "grey", "white"];
+
+
+var color = "black"
+var width = "10px"
+var height = "10px"
 
 
 var canvas = document.getElementById("canvas");
@@ -14,27 +18,48 @@ for (var i = 0; i < pickedColor.length; i++) {
     })
 }
 
+var changeBrushSize = document.getElementsByClassName("brushSize");
+for (var i = 0; i < changeBrushSize.length; i++) {
+    changeBrushSize[i].addEventListener('click', function (e) {
+        console.log(e.target.value)
+        width = e.target.value
+        height = e.target.value
+    })
+}
+
+var brushShapes = document.getElementsByClassName("brushShape");
+for (var i = 0; i < brushShapes.length; i++) {
+    brushShapes[i].addEventListener('click', function (e) {
+    radius = e.target.value
+})
+}
+
 
 var Start = function () {
     canvas.addEventListener("mousemove", function (e) {
         drawOnCanvas(e)
-
     });
 }
 
 var color = "black"
+var width = "10"
+var height = "10"
+var radius = "0%";
 
 var drawOnCanvas = function (e) {
     console.log(e);
     var dot = document.createElement("div");
-    dot.style.left = e.clientX - e.target.offsetLeft + "px";
-    dot.style.top = e.clientY - e.target.offsetTop + "px";
-    dot.style.width = "10px";
-    dot.style.height = "10px";
+    dot.style.left = e.pageX - e.target.offsetLeft + "px";
+    dot.style.top = e.pageY - e.target.offsetTop + "px";
+    dot.style.width =  width + "px";
+    dot.style.height = height + "px";
+    dot.style.borderRadius = radius;
     dot.style.position = "absolute";
     dot.style.backgroundColor = color;
     canvas.appendChild(dot);
 }
+
+
 
 document.getElementById("clearScreen").addEventListener("click", clearCanvas);
 
