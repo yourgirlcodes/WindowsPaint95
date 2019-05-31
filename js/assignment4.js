0c6b4a320b4c64551e3d224ceeee6efadade6fa1
Zoe Cohen <zoecohen01@gmail.com>
2019-05-31 14:35:17 +0200

    submission updates design eraser text

--- js/assignment4.js
+++ js/assignment4.js
@@ -10,40 +10,40 @@ var height = "10px"
 var canvas = document.getElementById("canvas");
 
 //picking color off color-palette:
-var pickedColor = document.getElementsByClassName("btn-group btn-lg");
+var pickedColor = document.getElementsByClassName("colorBtn btn-lg");
 for (var i = 0; i < pickedColor.length; i++) {
     pickedColor[i].addEventListener('click', function (e) {
-        console.log(e.target.id)
-        color = e.target.id
+        console.log(e.target.id);
+        color = e.target.id;
     })
 }
 
 var changeBrushSize = document.getElementsByClassName("brushSize");
 for (var i = 0; i < changeBrushSize.length; i++) {
     changeBrushSize[i].addEventListener('click', function (e) {
-        console.log(e.target.value)
-        width = e.target.value
-        height = e.target.value
+        console.log(e.target.value);
+        width = e.target.value;
+        height = e.target.value;
     })
 }
 
 var brushShapes = document.getElementsByClassName("brushShape");
 for (var i = 0; i < brushShapes.length; i++) {
     brushShapes[i].addEventListener('click', function (e) {
-    radius = e.target.value
-})
+        radius = e.target.value;
+    })
 }
 
 
 var Start = function () {
     canvas.addEventListener("mousemove", function (e) {
-        drawOnCanvas(e)
+        drawOnCanvas(e);
     });
 }
 
-var color = "black"
-var width = "10"
-var height = "10"
+var color = "black";
+var width = "10";
+var height = "10";
 var radius = "0%";
 
 var drawOnCanvas = function (e) {
@@ -51,7 +51,7 @@ var drawOnCanvas = function (e) {
     var dot = document.createElement("div");
     dot.style.left = e.pageX - e.target.offsetLeft + "px";
     dot.style.top = e.pageY - e.target.offsetTop + "px";
-    dot.style.width =  width + "px";
+    dot.style.width = width + "px";
     dot.style.height = height + "px";
     dot.style.borderRadius = radius;
     dot.style.position = "absolute";
